3c981cd1d41db94797fed87ea7b8485fbdb7004f
Rich Harris <richard.a.harris@gmail.com>
2017-08-05 20:20:53 -0400

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import buble from 'rollup-plugin-buble';
+import uglify from 'rollup-plugin-uglify';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	entry: 'src/main.js',
+	dest: 'public/bundle.js',
+	format: 'iife',
+	moduleName: 'app',
+	sourceMap: true,
+	plugins: [
+		svelte({
+			// we'll extract any component CSS out into
+			// a separate file — better for performance
+			css: css => {
+				css.write('public/bundle.css');
+			},
+
+			// this results in smaller CSS files
+			cascade: false
+		}),
+
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration —
+		// consult the documentation for details:
+		// https://github.com/rollup/rollup-plugin-commonjs
+		resolve(),
+		commonjs(),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), transpile and minify
+		production && buble({ exclude: 'node_modules/**' }),
+		production && uglify()
+	]
+};
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import App from './App.html';
+
+window.app = new App({
+	target: document.body,
+	data: {
+		name: 'world'
+	}
+});
