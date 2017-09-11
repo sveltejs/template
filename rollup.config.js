0ba461c53ad083e11630ee487947bdf33f4aae03
Omkar Patil <ospatil@gmail.com>
2017-09-11 09:56:10 -0400

    Add components build and publish support

--- rollup.config.js
+++ rollup.config.js
@@ -6,12 +6,32 @@ import uglify from 'rollup-plugin-uglify';
 
 const production = !process.env.ROLLUP_WATCH;
 
+const format = process.env.FORMAT || 'iife';
+const isUmd = format && format === 'umd';
+const isEs = format && format === 'es';
+
+const inputFile = (isUmd || isEs) ? 'src/components.js' : 'src/main.js';
+const outputCssFile = (isUmd || isEs) ? 'dist/bundle.css' : 'public/bundle.css';
+
+let outputFile;
+switch (format) {
+	case 'iife':
+		outputFile = 'public/bundle.js';
+		break;
+	case 'es':
+		outputFile = 'dist/bundle.es.js';
+		break;
+	case 'umd':
+		outputFile = 'dist/bundle.umd.js';
+		break;
+}
+
 export default {
-	input: 'src/main.js',	
+	input: inputFile,
 	output: {
-		sourcemap: true,	
-		format: 'iife',
-		file: 'public/bundle.js'
+		sourcemap: true,
+		format: format,
+		file: outputFile
 	},
 	name: 'app',
 	plugins: [
@@ -21,7 +41,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/bundle.css');
+				css.write(outputCssFile);
 			},
 
 			// this results in smaller CSS files
@@ -38,7 +58,7 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), transpile and minify
-		production && buble({ exclude: 'node_modules/**' }),
-		production && uglify()
+		production && isUmd && buble({ exclude: 'node_modules/**' }),
+		production && isUmd && uglify()
 	]
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import App from './App.html';
+import { App } from './components';
 
 const app = new App({
 	target: document.body,
