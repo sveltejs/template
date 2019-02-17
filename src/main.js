880b024bb4dcb86f15a3533d9cd54eec0f38d353
Bruce W. Clounie <bruce.w.clounie@gmail.com>
2019-02-16 21:34:42 -0700

    Add 'main' element to index.html so the template repo is consistent with the Getting Started guide + users won't get 'target required' error

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import App from './App.html';
 
 const app = new App({
-	target: document.body,
+	target: document.querySelector('main'),
 	data: {
 		name: 'world'
 	}
 });
 
-export default app;
+// change the data associated with the template
+app.set({ name: 'everybody' });
+
+// detach the component and clean everything up
+// commented out for example's sake - or you wouldn't see anything!
+// app.destroy();
