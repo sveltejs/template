3538f6dfaa14b79bb842a1eed7179c09d75ea84f
Jan Vladimir Mostert <jan.mostert@gmail.com>
2019-10-06 10:32:33 +0200

    fix for livereload not reloading from src directory
    live reload only works after the first change, then it stops working completely
    adding 'src' to the livereload config seems to fix that

--- rollup.config.js
+++ rollup.config.js
@@ -38,7 +38,7 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload(['src', 'public']),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
