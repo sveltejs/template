7b0bb3279dd1bc0fb5e60eccc65a3422395c8f62
Richard Harris <richard.a.harris@gmail.com>
2019-11-16 12:35:34 -0500

    remove start:dev, put serve plugin in config file

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
-import rollup_start_dev from './rollup_start_dev';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -37,9 +36,9 @@ export default {
 		}),
 		commonjs(),
 
-		// In dev mode, call `npm run start:dev` once
+		// In dev mode, call `npm run start` once
 		// the bundle has been generated
-		!production && rollup_start_dev,
+		!production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
@@ -53,3 +52,20 @@ export default {
 		clearScreen: false
 	}
 };
+
+function serve() {
+	let started = false;
+
+	return {
+		writeBundle() {
+			if (!started) {
+				started = true;
+
+				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+					stdio: ['ignore', 'inherit', 'inherit'],
+					shell: true
+				});
+			}
+		}
+	};
+}
