257f8e141e2fa34fd91ed56e5c37940a267a0667
Koffi Anderson Koffi <kandersonko@gmail.com>
2019-12-13 17:03:55 -0800

    switch to @rollup/plugin-node-resolve (#103)

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
