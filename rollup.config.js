8194bf8f6452f8117195bf304ac99b15d329fbb3
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-08-25 12:59:38 -0700

    Upgrade rollup-plugin-svelte to 6.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -42,7 +42,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file - better for performance
 			css: css => {
-				css.write('public/build/bundle.css');
+				css.write('bundle.css');
 			}
 		}),
 
