2e6189687848448ec43d808bf25b962a0594f875
Vincent Liao <vincent.liao@protonmail.com>
2020-11-04 09:56:44 +0700

    add scss support for global.css

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import scss from 'rollup-plugin-scss'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -56,6 +57,9 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
+		scss({
+			output: './public/global.css'
+		}),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import App from './App.svelte';
+import '../public/global.scss'
 
 const app = new App({
 	target: document.body,
