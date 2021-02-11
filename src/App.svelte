842baf4c81c0f928789d014922c08dae29b959b5
1seok2 <misohaha31@naver.com>
2021-02-11 23:32:20 +0900

    Open svelte tutorial in a new window

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Visit the <a href="https://svelte.dev/tutorial/basics" target="_blank">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
