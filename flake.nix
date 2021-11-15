0645281cc582df0bb7aee6e38d6cdccf39132f65
John Rinehart <johnrichardrinehart@gmail.com>
2021-11-15 13:39:42 +0200

    feat(/flake.nix): use github:numtide/flake-utils

--- flake.nix
+++ flake.nix
@@ -1,11 +1,21 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
   };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  rec {
+    packages = flake-utils.lib.flattenTree {
+      hello = pkgs.hello;
+    };
+
+    defaultPackage = packages.hello;
+  });
 }
